0ef37ddae13455a52f7a1607a8ace2527e38b2fe
Andres Castaneda <me@andresrocks.mx>
2024-11-03 10:40:21 +0000

    Added andresrocksmx aliases

--- profile-scripts/andresrocksmx-aliases.ps1
+++ profile-scripts/andresrocksmx-aliases.ps1
@@ -1,10 +1,18 @@
 function gpip() { (Invoke-WebRequest http://ifconfig.me/ip).Content }
 
-function sproj { Set-Location -Path $HOME\projects }
+$projectDirectory = "$env:USERPROFILE\projects"
+if (Test-Path $projectDirectory )
+{
+    New-PSDrive -Name Projects -PSProvider FileSystem -Root $projectDirectory  -Description "Projects directory"
+}
 
-function oproj { explorer -Path $HOME\projects }
+function proj { return $projectDirectory }
 
-function lproj { Get-ChildItem -Path $HOME\projects -Force }
+function sproj { Set-Location -Path $projectDirectory }
+
+function oproj { explorer -Path $projectDirectory }
+
+function lproj { Get-ChildItem -Path $projectDirectory -Force }
 
 function slproj { 
     sproj
@@ -19,3 +27,25 @@ function reload-profiles {
     . $PROFILE.CurrentUserAllHosts
     . $PROFILE
 }
+
+#############
+
+# Find out if the current user identity is elevated (has admin rights)
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If so and the current host is a command line, then change to red color 
+# as warning to user that they are operating in an elevated context
+if (($host.Name -match "ConsoleHost") -and ($isAdmin))
+{
+     $host.UI.RawUI.BackgroundColor = "DarkRed"
+     $host.PrivateData.ErrorBackgroundColor = "White"
+     $host.PrivateData.ErrorForegroundColor = "DarkRed"
+     Clear-Host
+}
+
+# Compute file hashes - useful for checking successful downloads 
+function md5    { Get-FileHash -Algorithm MD5 $args }
+function sha1   { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
